901e98f8865a224555c922421160ae20e157257a
Peretz <peretzman@gmail.com>
2017-09-28 11:55:05 -0500

    Added helper.js with html tags that are going to be reused, and also added JSON object to represent cat's info and replace it dynamically for the first cat.

---
+++ js/helper.js
@@ -0,0 +1,3 @@
+var HTMLCatName = '<div class="cat-name">Me llamo %cat-name%!</div>';
+var HTMLCatImage = '<img id="%cat-id%-image" class="cat-image" src="%cat-image%" alt="Cat picture">';
+var HTMLCatClickCounter = '<div id="%cat-id%-click-counter" class="click-counter">Count: 0</div> <hr>';
--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-var counter = 0;
-$('#cat-image').click(function(){
-   counter = counter + 1;
-   $('#click-counter').html("Count: " + counter);
+// First cat
+var cat1 = {
+    "name" : "Pedrito",
+    "id" : "cat1",
+    "image" : "images/cat1.jpg"
+}
+
+cat1.display = function() {
+    var formattedName = HTMLCatName.replace("%cat-name%", cat1.name);
+    var formattedImage = HTMLCatImage.replace("%cat-id%", cat1.id);
+    formattedImage = formattedImage.replace("%cat-image%", cat1.image);
+    var formattedClickCounter = HTMLCatClickCounter.replace("%cat-id%", cat1.id);
+
+    $('#cats-info').append(formattedName);
+    $('#cats-info').append(formattedImage);
+    $('#cats-info').append(formattedClickCounter);
+}
+
+cat1.display();
+
+var counter1 = 0;
+$('#cat1-image').click(function(){
+   counter1 = counter1 + 1;
+   $('#cat1-click-counter').html("Count: " + counter1);
+});
+
+var counter2 = 0;
+$('#cat2-image').click(function(){
+   counter2 = counter2 + 1;
+   $('#cat2-click-counter').html("Count: " + counter2);
 });
