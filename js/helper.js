c028fa992f0375cd81f2734c77ca17cd0ac0fadc
Peretz <peretzman@gmail.com>
2017-09-28 14:58:45 -0500

    Minor edits to the layout.

--- js/helper.js
+++ js/helper.js
@@ -1,4 +1,4 @@
-var HTMLCatListItem = '<li id="%cat-id%-list-item">%cat-name%</li>';
+var HTMLCatListItem = '<li id="%cat-id%-list-item"><u>%cat-name%</u></li>';
 var HTMLCatName = '<div class="cat-name">Me llamo %cat-name%!</div>';
 var HTMLCatImage = '<img id="%cat-id%-image" class="cat-image" src="%cat-image%" alt="Cat picture">';
 var HTMLCatClickCounter = '<div id="%cat-id%-click-counter" class="click-counter">Count: %cat-counter%</div> <hr>';
