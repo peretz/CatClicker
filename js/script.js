fc27808c490743fd10357bb2356148f9f9316ca1
Peretz <peretzman@gmail.com>
2017-10-03 11:57:13 -0500

    Reorganized code so that it follows the Model-View-Octopus model. This helps keeping the model and view duties separate.

--- js/script.js
+++ js/script.js
@@ -1,76 +1,109 @@
-var cats = [
-    {
-        "name" : "Pedrito",
-        "id" : "cat1",
-        "image" : "images/cat1.jpg",
-        "counter" : 0
-    },
-    {
-        "name" : "Juanito",
-        "id" : "cat2",
-        "image" : "images/cat2.jpg",
-        "counter" : 0
-    },
-    {
-        "name" : "Luis",
-        "id" : "cat3",
-        "image" : "images/cat3.jpg",
-        "counter" : 0
-    },
-    {
-        "name" : "Mario",
-        "id" : "cat4",
-        "image" : "images/cat4.jpg",
-        "counter" : 0
-    },
-    {
-        "name" : "Chuchito",
-        "id" : "cat5",
-        "image" : "images/cat5.jpg",
-        "counter" : 0
-    },
-    {
-        "name" : "Kevin",
-        "id" : "cat6",
-        "image" : "images/cat6.jpg",
-        "counter" : 0
+$(function(){
+
+    var model = {
+        currentCat: null,
+        cats: [
+            {
+                "name" : "Pedrito",
+                "id" : "cat1",
+                "image" : "images/cat1.jpg",
+                "counter" : 0
+            },
+            {
+                "name" : "Juanito",
+                "id" : "cat2",
+                "image" : "images/cat2.jpg",
+                "counter" : 0
+            },
+            {
+                "name" : "Luis",
+                "id" : "cat3",
+                "image" : "images/cat3.jpg",
+                "counter" : 0
+            },
+            {
+                "name" : "Mario",
+                "id" : "cat4",
+                "image" : "images/cat4.jpg",
+                "counter" : 0
+            },
+            {
+                "name" : "Chuchito",
+                "id" : "cat5",
+                "image" : "images/cat5.jpg",
+                "counter" : 0
+            },
+            {
+                "name" : "Kevin",
+                "id" : "cat6",
+                "image" : "images/cat6.jpg",
+                "counter" : 0
+            }
+        ]
     }
-]
 
-var displayListOfCats = function(){
-    for (var i = 0; i < cats.length; i++) {
-        var formattedListItem = HTMLCatListItem.replace("%cat-id%", cats[i].id);
-        formattedListItem = formattedListItem.replace("%cat-name%", cats[i].name);
+    var octopus = {
+        init: function(){
+            model.currentCat = model.cats[0];
 
-        $('#cats-list').append(formattedListItem);
+            viewCat.init();
+            viewListOfCats.init();
+        },
 
-        $('#' + cats[i].id + '-list-item').click((function(cat){
-            return function() {
-                displayCat(cat);
-            };
-        })(cats[i]));
+        getCats: function() {
+            return model.cats; 
+        },
+
+        getCurrentCat: function() {
+            return model.currentCat;
+        },
+
+        setCurrentCat: function(cat) {
+            model.currentCat = cat;
+        },
+
+        increaseCounter: function() {
+            model.currentCat.counter++;
+            viewCat.render();
+        }
     }
-}
 
-displayListOfCats();
+    var viewCat = {
+        init: function() {
+            // Add click counter.
+            $('#cat-image').click(function(){
+                octopus.increaseCounter();
+            });
 
-var displayCat = function(cat) {
-    var formattedName = HTMLCatName.replace("%cat-name%", cat.name);
-    var formattedImage = HTMLCatImage.replace("%cat-id%", cat.id);
-    formattedImage = formattedImage.replace("%cat-image%", cat.image);
-    var formattedClickCounter = HTMLCatClickCounter.replace("%cat-id%", cat.id);
-    formattedClickCounter = formattedClickCounter.replace("%cat-counter%", cat.counter);
+            this.render();
+        },
+        render: function() {
+            var currentCat = octopus.getCurrentCat();
+            $('#cat-name').html("Me llamo " + currentCat.name + "!");
+            $('#cat-image').attr("src", currentCat.image);
+            $('#cat-click-counter').html("Count: " + currentCat.counter);
+        }
+    }
 
-    // Add HTML code
-    var formattedCatInfo = formattedName + formattedImage + formattedClickCounter;
-    $('#cat-info').html(formattedCatInfo);
+    var viewListOfCats = {
 
-    // Add Click counter.
-    $('#' + cat.id + '-image').click(function(){
-       cat.counter = cat.counter + 1;
-       $('#' + cat.id + '-click-counter').html("Count: " + cat.counter);
-    });
-}
+        init: function() {
+            $('#cats-list').html("");
 
-// Display first cat.
-displayCat(cats[0]);
+            octopus.getCats().forEach(function(cat) {
+                var elem = document.createElement('li');
+                elem.textContent = cat.name;
+                elem.addEventListener('click', (function(localCat) {
+                    return function() {
+                        octopus.setCurrentCat(localCat);
+                        viewCat.render();
+                    };
+                })(cat));
+
+                $('#cats-list').append(elem);
+            });
+        }
+    };
+
+    octopus.init();
+});
